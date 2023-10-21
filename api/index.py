881258fd6fdcb312c55cb59c9b3b95a40df9df96
Shray Jain <sbjain2004@gmail.com>
2023-10-21 13:45:25 -0500

    Added login/signup endpoints

--- api/index.py
+++ api/index.py
@@ -1,6 +1,45 @@
 from flask import Flask
-app = Flask(__name__)
+from flask import request
+import certifi
 
-@app.route("/api/python")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+app = Flask(__name__)
+db_client = None
+
+@app.route("/create_user", methods=['POST'])
+def create_user():
+    db = get_db()
+    user_data = request.get_json()
+
+    first_name = user_data["first_name"]
+    last_name = user_data["last_name"]
+    username = user_data["username"]
+    password = user_data["password"]
+    if db["users"].find_one({"username": username}) is not None:
+        return {"success": False, "message": "Username already in use"}
+    inserted = db["users"].insert_one({"first_name": first_name, "last_name": last_name,
+                                       "username": username, "password": password})
+    return {"success": True, "user_id": str(inserted.inserted_id)}
+
+@app.route("/login", methods=['POST'])
+def login():
+    db = get_db()
+    user_data = request.get_json()
+    username = user_data["username"]
+    password = user_data["password"]
+
+    user = db["users"].find_one({"username": username})
+    if user is None or user["password"] != password:
+        return {"success": False}
+
+    return {"success": True}
+
+
+def get_db():
+    global db_client
+    if not db_client:
+        uri = "mongodb+srv://sbjain:OXALjd8aL4Z8OhtM@pfindb.2a6otno.mongodb.net/?retryWrites=true&w=majority"
+        db_client = MongoClient(uri, tlsCAFile=certifi.where())
+    return db_client.pfin_db
