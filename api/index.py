5cd43b4d955fd434f5877d38a3bafbe86ed52614
Surya Sunkari <suryasunkari@gmail.com>
2023-10-21 12:25:13 -0500

    sign in and sign up almost done

--- api/index.py
+++ api/index.py
@@ -1,6 +1,19 @@
-from flask import Flask
+import json
+from flask import Flask, request, jsonify
+from datetime import datetime, timedelta, timezone
+
 app = Flask(__name__)
+
 
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/sign_in_test")
+def sign_in_test():
+    response_body = {
+        "success": True,
+        "user_id" : 34
+    }
+
+    return response_body
