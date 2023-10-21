34a331d1b20e38481aaaafbd7d475c1dc609685f
Surya Sunkari <suryasunkari@gmail.com>
2023-10-21 15:39:01 -0500

    home page done

--- app/page.tsx
+++ app/page.tsx
@@ -6,53 +6,68 @@ import axios from 'axios';
 import jwt from 'jsonwebtoken';
 import { Router } from "next/router";
 import Link from "next/link";
+import {AiOutlineStock} from 'react-icons/ai';
+import {BiSolidBadgeDollar} from 'react-icons/bi';
 
-
-const Login = () => {
+const Home = () => {
   const router = useRouter();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
   
-  const handleSignIn = async () => {
-    console.log("handling sign in");
-    console.log(`Email: ${email}, Password: ${password}`);
+  const handleExpenseTrackerClick = () => {
+    console.log("routing to expense tracker");
+    router.push('/expenses');
+  };
+
+  const handleInvestmentsTrackerClick = () => {
+    console.log("routing to investments tracker");
+    router.push('/investments');
   }
 
   return (
-    <div className="flex h-screen w-screen justify-center items-center m-0 p-0 bg-gradient-to-tr from-blue-100 via-orange-100 to-orange-200">
-      <div className="w-[800px] h-[500px] min-h-min min-w-min bg-stone-50 p-6 rounded-3xl shadow-lg flex flex-col justify-evenly ">
-        <div>
-            <div className="text-center">
-                <h1 className=" text-xl text-sky-900 font-bold">Welcome back!</h1>
-            </div>
-            <div className="text-center">
-                <h1 className=" text-4xl text-sky-900 font-bold">Sign in</h1>
-            </div>
-        </div>
-        <div className="mb-4 mx-20">
-            <label className="block text-sm font-semibold mb-2 text-black dark:text-white">Email:</label>
-            <input type="email" className="py-3 px-4 block w-full border border-stone-300 rounded-full text-sm shadow-lg" 
-                value={email} onChange={(e) => setEmail(e.target.value)} 
-            />
-        </div>
-        <div className="mb-6 mx-20">
-            <label className="block text-sm font-semibold mb-2 text-black dark:text-white">Password:</label>
-            <input type="password" className="py-3 px-4 block w-full border border-stone-300 rounded-full text-sm shadow-lg" 
-                value={password} onChange={(e) => setPassword(e.target.value)} 
-            />
-        </div>
-        <div className=" flex flex-col justify-center items-center">
-            <button
-                className="bg-sky-900 text-white rounded-full py-2 px-4 hover:bg-sky-700 transition duration-300"
-                onClick={handleSignIn}
-            >
-                <h2 className=" mx-8 font-semibold">Login</h2>
-            </button>
-            <p className="pt-4">Don't have an account? <Link className=" text-blue-700"  href="/signUp">Sign up</Link> instead.</p>
-            </div>
-        </div>
+    <div className="h-screen flex flex-col justify-center items-center bg-gradient-to-r from-slate-800 via-slate-700 to-slate-800">
+      <div className=" h-5/6 flex flex-col w-screen justify-center items-center m-0 p-0">
+          <div className=" px-8 flex flex-col justify-center items-center">
+              <div className="flex flex-col justify-center items-center">
+                <p className=" text-5xl font-bold text-rose-400 font-mono pb-4 text-center">Cash Guardian: Your Personal Finance Accountability Tracker</p>
+                <p className=" text-3xl font-bold text-rose-300 font-mono pb-4">Let's get started!</p>
+              </div>
+              <div className="max-w-[85rem] px-4 sm:px-6 lg:px-8 lg:py-14 mx-auto     ">
+                  <div className="grid grid-cols-2 gap-52">
+
+                      <div className="group flex flex-col h-full bg-white shadow-xl rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]  hover:scale-110 transition cursor-pointer" onClick={handleExpenseTrackerClick}>
+                          <div className="h-52 flex flex-col justify-center items-center bg-green-600 rounded-t-xl">
+                              <BiSolidBadgeDollar size={200} />
+                          </div>
+                          <div className="p-4 h-full flex flex-col justify-evenly items-center">
+                              <div className="block mb-1 text-xs font-semibold uppercase text-green-600">
+                                Get personalized feedback on your spending habits!
+                              </div>
+                              <div className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                                Expense Tracker
+                              </div>
+                          </div>
+                      </div>
+
+                      <div className="group flex flex-col h-full bg-white shadow-xl rounded-xl dark:bg-slate-900 dark:border-gray-700 dark:shadow-slate-700/[.7]  hover:scale-110 transition cursor-pointer" onClick={handleInvestmentsTrackerClick}>
+                          <div className="h-52 flex flex-col justify-center items-center bg-blue-600 rounded-t-xl">
+                              <AiOutlineStock size={200} />
+                          </div>
+                          <div className="p-4 h-full flex flex-col justify-evenly items-center">
+                              <div className="block mb-1 text-xs font-semibold uppercase text-blue-600">
+                                Gain insights related to your investments portfolio!
+                              </div>
+                              <div className="text-xl font-semibold text-gray-800 dark:text-gray-300 dark:hover:text-white">
+                                Investments Tracker
+                              </div>
+                          </div>
+                      </div>
+          
+                  </div>
+              </div>
+          </div>
+      </div>
+      <h3 className=" text-white font-semibold pt-4">Created for HackTX by Shray Jain, Surya Sunkari, and Tarun Mohan</h3>
     </div>
   );
 };
 
-export default Login;
+export default Home;
