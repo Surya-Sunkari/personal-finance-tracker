0b05fdf9797c7353a467ecbffc38253456b2233d
Surya Sunkari <suryasunkari@gmail.com>
2023-10-22 02:39:50 -0500

    Add Expense button done

--- components/AddExpense.tsx
+++ components/AddExpense.tsx
@@ -7,6 +7,11 @@ import Fade from '@mui/material/Fade';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import { useRouter } from "next/navigation";
+import axios from 'axios';
+import jwt from 'jsonwebtoken';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const style = {
     position: 'absolute' as 'absolute',
@@ -28,17 +33,60 @@ const AddExpense = () => {
     const [expenseName, setExpenseName] = useState('');
     const [expenseCost, setExpenseCost] = useState('');
     const [expenseDate, setExpenseDate] = useState('');
+    const [userId, setUserId] = useState('');
+
+    const router = useRouter();
+
+    useEffect(() => {
+        const token = localStorage.getItem('jwt_token');
+        const user_id = jwt.decode(token).user_id;
+        setUserId(user_id);
+    }, []);
 
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
     const handleSubmit = async () => {
-        console.log(expenseName, expenseCost, expenseDate);
-        console.log("submit");
+        try {
+            const user_data = {
+                user_id: userId,
+                name: expenseName,
+                cost: parseInt(expenseCost),
+                date: expenseDate,
+            };
+        
+            const response = await axios.post('http://127.0.0.1:5328/new_expense', user_data);
+        
+            if (response.status === 200) {
+                if(response.data.success) {
+                    console.log("expense added"); // TODO: add toast
+                    setExpenseCost('');
+                    setExpenseDate('');
+                    setExpenseName('');
+                    handleClose();
+                    toast.success("Expense added!", {
+                        position: toast.POSITION.TOP_CENTER
+                    });
+                } else {
+                    console.error('Expense Adding Failed', response.data); 
+                }
+            } else {
+                toast.error("Error!", {
+                    position: toast.POSITION.TOP_CENTER
+                });
+                console.error('Error:', response.data);
+            }
+        } catch (error) {
+            toast.success("Error!", {
+                position: toast.POSITION.TOP_CENTER
+            });
+            console.error('Network/Request Error:', error);
+        }
     };
 
     return (
         <div>
-            <Button variant='contained' onClick={handleOpen}>Manually Add Expense</Button>
+            <ToastContainer />
+            <Button variant="contained" onClick={handleOpen} className="bg-blue-600">Manually Add Expense</Button>
             <Modal
                 aria-labelledby="transition-modal-title"
                 aria-describedby="transition-modal-description"
@@ -68,7 +116,7 @@ const AddExpense = () => {
                             <input type="text" id="input-label" className="py-3 px-4 block w-full border-2 border-gray-200 rounded-md text-sm focus:border-blue-500 focus:ring-blue-500" placeholder="2023-10-21" value={expenseDate} onChange={(e) => setExpenseDate(e.target.value)}></input>
                         </div>
                         <div className='pt-3 flex justify-center items-center'>
-                            <Button variant='contained' onClick={handleSubmit}>Submit</Button>
+                            <Button variant='contained' color="success" className="bg-green-800"  onClick={handleSubmit}>Submit</Button>
                         </div>
                     </Box>
                 </Fade>
